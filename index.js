10087c395cef30c7b5a5289e5e84f27727bd6427
drinkeewu <773119690@qq.com>
2020-02-04 21:21:10 +0800

    feat: 新增地图组件

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 export { default as WordCloudChart } from './packages/word-cloud';
 export { default as BarChart } from './packages/bar';
+export { default as Map } from './packages/map';
---
+++ packages/map/src/Map.vue
@@ -0,0 +1,216 @@
+<template>
+  <vchart
+    autoresizse
+    :options="options"
+    :style="style"
+  />
+</template>
+
+<script>
+import echarts from 'echarts';
+import 'echarts/map/js/china';
+import 'echarts/extension/bmap/bmap';
+import 'echarts/map/js/province/guangdong';
+import vchart from 'vue-echarts';
+import { GUANGDONG_CITY_MAP } from 'shared/constants';
+
+
+const GD_CITY_NAMES = Object.keys(GUANGDONG_CITY_MAP);
+
+function resolveJSON(city) {
+  // eslint-disable-next-line import/no-dynamic-require
+  return require(`./json/${city}.json`);
+}
+
+export default {
+  name: 'Map',
+  components: {
+    vchart,
+  },
+  props: {
+    width: {
+      type: String,
+      default: '100%',
+    },
+    height: {
+      type: String,
+      default: '100%',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    data: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    mapType: {
+      type: String,
+      default: 'china',
+    },
+    titleColor: {
+      type: String,
+      default: '#000',
+    },
+    titlePosition: {
+      type: String,
+      default: 'center',
+    },
+    itemColor: {
+      type: String,
+      default: '#f4e925',
+    },
+    itemShadowColor: {
+      type: String,
+      default: '#f4e925',
+    },
+    itemMinSize: {
+      type: Number,
+      default: 3,
+    },
+    itemMaxSize: {
+      type: Number,
+      default: 60,
+    },
+    areaColor: {
+      type: String,
+      default: '#eaeaea',
+    },
+    borderColor: {
+      type: String,
+      default: '#999',
+    },
+    type: {
+      type: String,
+      default: 'map',
+    },
+
+    areaEmphasisColor: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: 'china',
+    },
+
+    roam: {
+      type: Boolean,
+      default: false,
+    },
+    zoom: {
+      type: Number,
+      default: 1,
+    },
+    center: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    autoTooltip: {
+      type: Boolean,
+      default: false,
+    },
+    lowColor: {
+      type: String,
+      default: '#ffd65a',
+    },
+    highColor: {
+      type: String,
+      default: '#fa675c',
+    },
+    backgroundColor: {
+      type: String,
+      default: 'transparent',
+    },
+    labelColor: {
+      type: String,
+      default: '#333',
+    },
+  },
+  computed: {
+    style() {
+      const { width, height } = this;
+      return {
+        width,
+        height,
+      };
+    },
+    options() {
+      const { data } = this;
+      const MIN = data.length > 0
+        ? data.length > 1
+          ? Math.min(...(data.map(i => i.value)))
+          : 0
+        : 0;
+      const MAX = data.length > 0
+        ? Math.max(...(data.map(i => i.value)))
+        : 0;
+      const options = {
+        visualMap: {
+          type: 'continuous',
+          orient: 'horizontal',
+          itemWidth: 10,
+          itemHeight: 80,
+          min: MIN,
+          max: MAX,
+          text: ['高', '低'],
+
+          inRange: {
+            color: [this.lowColor, this.highColor],
+          },
+          textStyle: {
+            color: this.titleColor,
+          },
+          right: '15px',
+          bottom: '15px',
+        },
+        series: [
+          {
+            name: this.name,
+            type: 'map',
+            mapType: this.mapType,
+            roam: this.roam,
+            itemStyle: {
+              normal: {
+                areaColor: this.areaColor,
+                borderColor: this.borderColor,
+                label: {
+                  show(params) {
+                    console.log('show', params);
+                    return true;
+                  },
+                  textStyle: {
+                    color: this.labelColor,
+                  },
+                },
+              },
+              emphasis: {
+                label: { show: true },
+              },
+
+            },
+            label: {
+              emphasis: {
+                textStyle: {
+                  color: this.labelColor,
+                },
+              },
+            },
+            data,
+            zlevel: 1,
+
+          },
+        ],
+      };
+      return options;
+    },
+  },
+  methods: {
+
+  },
+};
+</script>
